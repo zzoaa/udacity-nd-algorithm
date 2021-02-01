435890c673e81589b6f69fe1f1a12c55c76f5003
skcho <skcho@spectra.co.kr>
2021-02-02 07:17:13 +0900

    project2 - problem_5.py array -> linkedlist and testcase update

--- project2/problem_5.py
+++ project2/problem_5.py
@@ -2,6 +2,55 @@ import hashlib
 import time
 
 
+class Node(object):
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+    def __str__(self):
+        return "({},{})".format(self.key, self.value)
+
+    def __repr__(self):
+        return "({} <- ({},{}) ->{})".format(self.prev, self.key, self.value, self.next)
+
+
+class DoubleLinkedList(object):
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def get_tail(self):
+        return self.tail
+
+    def pop(self):
+
+        result = self.head
+        self.head = result.next
+        return result
+
+    def append(self, node):
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            node.prev = self.tail
+            self.tail.next = node
+            self.tail = node
+
+    def __repr__(self):
+        current = self.head
+        result = []
+        while current:
+            result.append(current)
+            current = current.next
+
+        return "\n".join([str(item) for item in result])
+
+
 class Block:
 
     def __init__(self, timestamp, data, previous_hash):
@@ -9,6 +58,8 @@ class Block:
         self.data = data
         self.previous_hash = previous_hash
         self.hash = self.calc_hash()
+        self.next = None
+        self.prev = None
 
     def calc_hash(self):
         sha = hashlib.sha256()
@@ -24,18 +75,17 @@ class Block:
 class BlockChain(object):
 
     def __init__(self):
-        self.blocks = []
+        self.blocks = DoubleLinkedList()
 
     def add(self, data):
         prev_hash = None
-        if self.blocks:
-            prev_hash = self.blocks[-1].hash
+        if self.blocks.get_tail():
+            prev_hash = self.blocks.get_tail().hash
 
         self.blocks.append(Block(time.gmtime(), data, prev_hash))
 
     def print_all(self):
-        for block in self.blocks:
-            print(block)
+        print(self.blocks)
 
 
 def main():
@@ -46,6 +96,26 @@ def main():
     blocks.add("3")
 
     blocks.print_all()
+    print("="*10)
 
+    blocks.add("4")
+    blocks.add("5")
+    blocks.add("6")
+    blocks.add("4")
+    blocks.add("5")
+    blocks.add("6")
+    blocks.add("4")
+    blocks.add("5")
+    blocks.add("6")
+    blocks.add("4")
+    blocks.add("5")
+    blocks.add("6")
+
+    blocks.print_all()
+    print("=" * 10)
+
+    blocks = BlockChain()
+    blocks.print_all()
+    print("=" * 10)
 
 main()
